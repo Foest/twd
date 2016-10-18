b095206fc6d4a41b142b7be759677889afce8ccc
foest <foest88@gmail.com>
2016-10-18 11:10:44 -0700

    added category url pattern

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,6 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
-    url('', views.default, name='default'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
 ]
