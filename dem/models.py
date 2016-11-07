8a7743fe4e02fe0e7998f0fd5b81383c4daac5b1
foest <foest88@gmail.com>
2016-11-07 12:42:11 -0800

    created initial DemUser model

--- dem/models.py
+++ dem/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from localflavor.us.models import USStateField
+
+class DemUser(models.Model):
+    user = models.OneToOneField(User)
+    state = USStateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Dem Users'
