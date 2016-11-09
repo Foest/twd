4341386bf4e2d9dad063f679cee36f57136f6a98
foest <foest88@gmail.com>
2016-11-09 11:28:22 -0800

    added proposal model to admin page

--- dem/admin.py
+++ dem/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from dem.models import DemUser
+from dem.models import DemUser, Proposal
+
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = ['title','author', 'date_created']
+    prepopulated_fields = {'slug':('title',)}
+    fields = ['title', 'content', 'author', 'slug']
 
 admin.site.register(DemUser)
+admin.site.register(Proposal, ProposalAdmin)
