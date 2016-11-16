1b765c9f557458b876edeb7a417bf0a36ed3475c
foest <foest88@gmail.com>
2016-11-16 09:44:32 -0800

    added registration redux vars

--- twd/settings.py
+++ twd/settings.py
@@ -131,4 +131,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/rango/login/'
+LOGIN_URL = '/accounts/login/'
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
