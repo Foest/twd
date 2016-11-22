25cd97aa23ce4e34d04ded755d529cd5db4a1cfd
foest <foest88@gmail.com>
2016-11-22 11:23:55 -0800

    updated admin to display Reward

--- dem/admin.py
+++ dem/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from dem.models import DemUser, Proposal, Mission
+from dem.models import DemUser, Proposal, Mission, Reward
 
 class ProposalAdmin(admin.ModelAdmin):
     list_display = ['title','author', 'date_created']
@@ -9,8 +9,13 @@ class ProposalAdmin(admin.ModelAdmin):
 class MissionAdmin(admin.ModelAdmin):
     list_display = ['title','creator', 'date_created', 'lat_coord', 'lng_coord']
     prepopulated_fields = {'slug':('title',)}
-    fields = ['title', 'creator', 'lat_coord', 'lng_coord', 'description', 'slug']
+    fields = ['title', 'creator', 'lat_coord', 'lng_coord', 'description', 'reward', 'slug']
+
+class RewardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'monies']
+    fields = ['title', 'monies']
 
 admin.site.register(DemUser)
+admin.site.register(Reward, RewardAdmin)
 admin.site.register(Mission, MissionAdmin)
 admin.site.register(Proposal, ProposalAdmin)
