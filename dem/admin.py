66221a91a808495e9b7de46d1890558fc30f2a28
foest <foest88@gmail.com>
2016-11-23 09:06:40 -0800

    added Assignment display in admin, updated Mission admin display

--- dem/admin.py
+++ dem/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from dem.models import DemUser, Proposal, Mission, Reward
+from dem.models import DemUser, Proposal, Mission, Reward, Assignment
 
 class ProposalAdmin(admin.ModelAdmin):
     list_display = ['title','author', 'date_created']
@@ -7,15 +7,20 @@ class ProposalAdmin(admin.ModelAdmin):
     fields = ['title', 'content', 'author', 'slug']
 
 class MissionAdmin(admin.ModelAdmin):
-    list_display = ['title','creator', 'date_created', 'lat_coord', 'lng_coord']
+    list_display = ['title','creator', 'date_created']
     prepopulated_fields = {'slug':('title',)}
-    fields = ['title', 'creator', 'lat_coord', 'lng_coord', 'description', 'reward', 'slug']
+    fields = ['title', 'creator', 'description', 'slug']
 
 class RewardAdmin(admin.ModelAdmin):
     list_display = ['title', 'monies']
     fields = ['title', 'monies']
 
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['assignee', 'mission', 'reward', 'lat_coord', 'lng_coord']
+    fields = ['assignee', 'mission', 'reward', 'lat_coord', 'lng_coord']
+
 admin.site.register(DemUser)
+admin.site.register(Assignment, AssignmentAdmin)
 admin.site.register(Reward, RewardAdmin)
 admin.site.register(Mission, MissionAdmin)
 admin.site.register(Proposal, ProposalAdmin)
